fdb2552140db7fc6514b929514aa782fe7c1b3f6
Florian <aichele.florian@web.de>
2024-10-01 10:09:29 +0200

    Added mapping to get prices

--- src/main/java/com/example/smartchargebackend/controller/ChargeTimesController.java
+++ src/main/java/com/example/smartchargebackend/controller/ChargeTimesController.java
@@ -14,22 +14,31 @@ public class ChargeTimesController {
     public List<PriceData> getChargeTimes(@RequestParam String id) {
         return TibberAPI.getChargingHours(id);
     }
+
     @GetMapping("/setchargetimestf")
     public List<PriceData> setChargeTimes(@RequestParam String id, @RequestParam Integer timeframe,
                                           @RequestParam Integer hours) {
         return TibberAPI.scheduleChargingHoursForId(id, timeframe, hours);
     }
+
     @GetMapping("/setchargetimes")
     public List<PriceData> setChargeTimes(@RequestParam String id,
                                           @RequestParam Integer hours) {
         return TibberAPI.scheduleChargingHoursForId(id, hours);
     }
+
     @GetMapping("/devices")
     public List<String> getDevices() {
         return TibberAPI.getDevices();
     }
+
     @GetMapping("/remove")
     public void remove(@RequestParam String id) {
         TibberAPI.removeDevice(id);
+    }
+
+    @GetMapping("/prices")
+    public List<PriceData> getPrices() {
+        return TibberAPI.getPriceList();
     }
 }
